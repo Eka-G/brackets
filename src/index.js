8679c55e2cab2a5dc3a71b0d63600210b329fedb
Eka-G <ekaglazkovaa@yandex.ru>
2021-02-25 22:46:42 +0300

    fix: implement function check (20/20 pass)

--- src/index.js
+++ src/index.js
@@ -2,42 +2,23 @@ module.exports = function check(str, bracketsConfig) {
   let checkIn = [];
   let res = true;
 
-  str.split("").map(item => {
-    switch (item) {
-      case "(":
-      case "[":
-      case "{":
+  str.split("").forEach(item => {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      if (bracketsConfig[i][0] === bracketsConfig[i][1] && bracketsConfig[i][0] === item) { //проверка на соответсвие одинаковых символов
+        checkIn.find(x => x === bracketsConfig[i][0]) ? checkIn.pop() : checkIn.push(item);
+        continue;
+      }
+
+      if (item === bracketsConfig[i][0]) {
         checkIn.push(item);
-        break;
-
-      case ")":
-        if (checkIn.length === 0) res = false;
-        if (checkIn[checkIn.length - 1] === "(") {
-          checkIn.pop()
-        } else res = false;
-        break;
-
-      case "]":
-        if (checkIn.length === 0) res = false;
-        if (checkIn[checkIn.length - 1] === "[") {
-          checkIn.pop()
-        } else res = false;
-        break;
-
-      case "}":
-        if (checkIn.length === 0) res = false;
-        if (checkIn[checkIn.length - 1] === "{") {
-          checkIn.pop()
-        } else res = false;
-        break;
-
-      case "|":
-        checkIn.filter(item => item === "|").length % 2 === 0 ? checkIn.push(item) :
-          checkIn[checkIn.length - 1] === "|" ?
-            checkIn.pop() : res = false;;
-        break;
+        continue;
+      } else if (item === bracketsConfig[i][1]) {
+        checkIn.find(repeat => repeat === bracketsConfig[i][1]) === true ? checkIn.push(item) :
+          checkIn[checkIn.length - 1] === bracketsConfig[i][0] ? checkIn.pop() : res = false;
+        continue;
+      }
     }
-  });
+  })
 
   return res === false ? false :
     checkIn.length === 0 ? res = true : res = false;
