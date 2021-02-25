1371662bfe4c7d5d3372a4474795a1d63ff96e24
Eka-G <ekaglazkovaa@yandex.ru>
2021-02-25 17:04:08 +0300

    feat: wrong solution without second arg (18/20 pass)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let checkIn = [];
+  let res = true;
+
+  str.split("").map(item => {
+    switch (item) {
+      case "(":
+      case "[":
+      case "{":
+        checkIn.push(item);
+        break;
+
+      case ")":
+        if (checkIn.length === 0) res = false;
+        if (checkIn[checkIn.length - 1] === "(") {
+          checkIn.pop()
+        } else res = false;
+        break;
+
+      case "]":
+        if (checkIn.length === 0) res = false;
+        if (checkIn[checkIn.length - 1] === "[") {
+          checkIn.pop()
+        } else res = false;
+        break;
+
+      case "}":
+        if (checkIn.length === 0) res = false;
+        if (checkIn[checkIn.length - 1] === "{") {
+          checkIn.pop()
+        } else res = false;
+        break;
+
+      case "|":
+        checkIn.filter(item => item === "|").length % 2 === 0 ? checkIn.push(item) :
+          checkIn[checkIn.length - 1] === "|" ?
+            checkIn.pop() : res = false;;
+        break;
+    }
+  });
+
+  return res === false ? false :
+    checkIn.length === 0 ? res = true : res = false;
 }
